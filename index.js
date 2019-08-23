2d8653e25608607f71b12176b6846e73590bf11d
mlearo <mlearo@gmail.com>
2019-08-23 11:04:42 -0700

    add function to index.js, add jsdoc

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-var people = [
-  { name: 'Michael Learo' }
-]
+var people = [{ name: 'Michael Learo' }]
 
 console.log(people[0].name)
+
+/**
+ * This is a function.
+ *
+ * @param {string} a - A string param
+ * @return {string} A good string
+ *
+ * @example
+ *
+ *     foo('hello')
+ */
+
+function foo(a) {
+  return a
+}
+
+foo('hello world')
