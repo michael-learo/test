53baef85da7250ddbaa936196ceccff3041bcbe8
mlearo <mlearo@gmail.com>
2019-09-07 12:37:32 -0700

    message

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
-var fs = require('fs')
-console.log(fs)
-var people = [{ name: 'Michael Learo' }]
+// var fs = require('fs')
+
+/**
+ * A variable in the global namespace.
+ * Array of N objects, each object is a person
+ * containing a first name, last name, age, and gender
+ *
+ * @var {array} people
+ * @example [{fistsName: "Bob", lastName: "Larry", age : 45, gender : "M"}]
+ */
+
+var people = [
+  {
+    firstName: 'Michael',
+    lastName: 'Learo',
+    age: 34,
+    gender: 'M',
+  },
+]
 
 console.log(people[0].name)
 
@@ -20,3 +36,14 @@ function foo(a) {
 }
 
 foo('hello world')
+
+/**
+ * @type {number}
+ */
+var FOO = 1
+console.log(FOO)
+/**
+ * @const {number}
+ */
+const ZOO = 1
+console.log(ZOO)
